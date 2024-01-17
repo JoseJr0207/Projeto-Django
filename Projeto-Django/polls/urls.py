e537d6dc0169e2bd0fd0006ec57c5cb26483e8e3
José Carlos <bronx.junior@hotmail.com>
2024-01-17 14:41:01 -0300

    Project_final

--- Projeto-Django/polls/urls.py
+++ Projeto-Django/polls/urls.py
@@ -8,5 +8,5 @@ urlpatterns = [
     path('home/', views.home, name='home'),
     path('<int:question_id>/results/', views.results, name='results'),
     path('<int:question_id>/vote/', views.vote, name='vote'),
-    path('<int:poll_id>/', views.detail, name='detail'),
+    path('<int:question_id>/', views.detail, name='detail'),  # Altere 'poll_id' para 'question_id'
 ]
---
+++ Projeto-Django/polls/views.py
@@ -0,0 +1,44 @@
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
+from .models import Poll
+from .models import Question
+
+
+def index(request):
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    latest_poll_list = Poll.objects.order_by('-pub_date')[:5]
+    context = {'latest_question_list': latest_question_list, 'latest_poll_list': latest_poll_list}
+    return render(request, 'polls/index.html', context)
+
+def home(request):
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    latest_poll_list = Poll.objects.order_by('-pub_date')[:5]
+    context = {'latest_question_list': latest_question_list, 'latest_poll_list': latest_poll_list}
+    return render(request, 'polls/home.html', context)
+
+
+
+def results(request, question_id):
+    question = Question(pk=question_id)
+    return render(request, 'polls/results.html', {'question': question})
+
+
+def detail(request, poll_id):
+    poll = get_object_or_404(Poll, pk=poll_id)
+    return render(request, 'polls/detail.html', {'poll': poll})
+
+
+def vote(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except KeyError:
+        return render(request, 'polls/vote.html', {
+            'question': question,
+            'error_message': "You  didn't select a choice.",
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
